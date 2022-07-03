44c2c855b8698632db38c399ac8b4fa0c7e08acf
MIINII <83333217+MIINII@users.noreply.github.com>
2022-07-03 23:23:16 +0900

    MODULE 겅부

---
+++ main.js
@@ -0,0 +1,4 @@
+// python import랑 비슷한듯? : 모듈을 로드해서 객체 하나를 리턴
+let m = require('./math_tools.js');
+
+console.log(m.add(1,2));
--- test.js
+++ test.js
@@ -1,1 +1,11 @@
+//  NODE REPL 모드
+// Read / Eval / Print / Loop
+// 사용자가 입력한 내용을 읽고 -> 그결괏값을 구한다음 -> 결괏값을 출력 -> 이런과정을 계속 반복
+// In >> node
+// exit >> .exit
+
 console.log("hello node.js");
+
+// module -> js 파일 중 하나 (전체를 이루는 파일 중 하나)
+
+function add(a, b)
